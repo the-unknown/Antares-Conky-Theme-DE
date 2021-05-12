95197be4491ae30e9e9d8cbe02563fe64c3979fd
the-unknown <junk@matthias-schlich.de>
2021-05-12 17:36:06 +0200

    Update weather.sh

--- conky/Antares/scripts/weather.sh
+++ conky/Antares/scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 api_key=1a574cba6c46aef984b2efbe3061b147
-city_id=2841463
+city_id=2886242
 url="api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${api_key}&lang=de "
 curl ${url} -s -o ~/.cache/eleg-weather.json
